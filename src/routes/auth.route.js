145a1a42ccd27060dd0ef5b822899e3b8f1907db
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-02-17 06:36:35 +0530

    Update auth.route.js

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -49,18 +49,18 @@ import { upload } from "../middleware/upload.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
-router.post("/logout", logout);
+router.post("/api/signup", signup);
+router.post("/api/login", login);
+router.post("/api/logout", logout);
 
 router.post(
-  "/onboarding",
+  "/api/onboarding",
   protectRoute,
   upload.single("profilePic"),
   onboard
 );
 
-router.get("/me", protectRoute, (req, res) => {
+router.get("/api/me", protectRoute, (req, res) => {
   res.status(200).json({
     success: true,
     user: req.user,
